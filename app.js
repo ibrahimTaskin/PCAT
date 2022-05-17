52b5f012282d4d8d366e9589fb123a0768574909
ibrahimTaskin <taskin.ibrahim@hotmail.com>
2022-05-17 06:34:50 +0300

    added temp files

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+//MIDDLEWARES
 app.use(express.static('public'));
 
 const port = 3000;
 
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.send(blog);
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
 
 app.listen(port, () => {
