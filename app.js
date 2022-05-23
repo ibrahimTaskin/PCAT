040f36b735d53a4c4d92d5d2a0aa11b8ec44811c
ibrahimTaskin <taskin.ibrahim@hotmail.com>
2022-05-23 08:17:13 +0300

    mongoose post processing

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
 
 const app = express();
+
+//connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const port = 3000;
 
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  console.log(photos);
+  res.render('index',{
+    photos
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -22,8 +36,14 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/add', (req, res) => {
-    res.render('add');
-  });
+  res.render('add');
+});
+
+// catch action
+app.post('/photos', async(req, res) => {
+  await Photo.create(req.body);  
+  res.redirect('/');
+});
 
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda baslatıldı`);
